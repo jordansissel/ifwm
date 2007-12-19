07c7a3f534a57e2f3a3c2e043d54dcc21d49891b
Jordan Sissel <jls@semicomplete.com>
2007-12-19 06:43:00 +0000

    git-svn-id: https://semicomplete.googlecode.com/svn/ifwm@1668 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ main.c
@@ -0,0 +1,12 @@
+// use bdb
+// store things in an in-memory bdb?
+
+/*
+ * provide a "window manager" library.
+ * - accept all necessary events
+ * + Allow registry of events
+ *  | Register events.
+ *  | Store list of windows, state, etc
+ * + Provide querying of window list without hitting the X server?
+ * + Store additional properties for all windows via optional struct.
+ */
