77519a3c38388a5c0cb402b5042019746afbe834
Jordan Sissel <jls@semicomplete.com>
2007-12-20 01:13:51 +0000

    git-svn-id: https://semicomplete.googlecode.com/svn/ifwm@1669 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- main.c
+++ main.c
@@ -10,3 +10,12 @@
  * + Provide querying of window list without hitting the X server?
  * + Store additional properties for all windows via optional struct.
  */
+
+typedef struct xenv {
+  
+} xenv_t
+
+wm_t *wm_init(char *display) {
+  wm_t *wm = NULL:
+  wm = malloc(sizeof(wm_t));
+}
